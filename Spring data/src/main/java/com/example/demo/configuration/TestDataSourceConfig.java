d8ed77f8a1c08258d97045f5634d1db7b24993e1
Bartosz Surma <surmab@tt.com.pl>
2018-03-08 16:08:32 +0100

    Spring data, configuration

---
+++ Spring data/src/main/java/com/example/demo/configuration/TestDataSourceConfig.java
@@ -0,0 +1,59 @@
+package com.example.demo.configuration;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.*;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Configuration
+@EnableTransactionManagement
+@Profile("test")
+public class TestDataSourceConfig {
+
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("testDataSource") DataSource dataSource) {
+        return builder.dataSource(dataSource)
+                .properties(hibernateJpaProperties())
+                .packages("com.example.demo.model")
+                .persistenceUnit("test")
+                .build();
+    }
+
+    private Map<String, ?> hibernateJpaProperties() {
+        HashMap<String, String> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("hibernate.show_sql", "false");
+        properties.put("hibernate.format_sql", "false");
+        properties.put("hibernate.ejb.naming_strategy", "org.hibernate.cfg.ImprovedNamingStrategy");
+        properties.put("hibernate.c3p0.min_size", "2");
+        properties.put("hibernate.c3p0.max_size", "5");
+        properties.put("hibernate.c3p0.timeout", "300"); // 5mins
+
+        return properties;
+    }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager(@Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+
+    @Bean(name = "testDataSource")
+    @ConfigurationProperties(prefix = "test.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
